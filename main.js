cfd00020c50a4284482cc86bf6415c1b1537aaf0
DIEGOFLOREZBELTRAN <diflorez@misena.edu.co>
2021-04-24 09:46:29 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+
+console.log("Ciclos");
+
+
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+// }
+
+// for (let i = ''; !(i == 'Diego');) {
+//     i = prompt("Ingrese su nombre");
+// }
+
+// let i = 0, x = 0;
+// while(i<5 && x<5){
+//     console.log(i);
+//     console.log(`Variable constante ${x}`);
+//     i++;
+// }
+
+// let opc = '';
+// while(!(opc=="Diego")){
+//     opc = prompt("Ingrese su nombre");
+// }
+
+// do{
+//     var opc = prompt("Ingrese su nombre");
+// }while(!(opc=="Diego"));
+
+
+// let papitas = 1;
+
+// while(!(papitas>5)){
+//     console.log(`Total de papitas ${papitas} `);
+//     papitas++;
+// } 
+
+// let papitas2 = 1;
+// do{
+//     console.log(`Total de papitas ${papitas2} `);
+//     papitas2++;
+// }while(!(papitas2>5));
+
